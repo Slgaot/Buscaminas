468b6b930f8782a8ffb48ef960644b2c039b107a
Slgaot <slobaton940@alumnes.politecnicllevant.cat>
2025-04-10 20:12:55 +0200

    Clase que representa una celda en el tablero

--- Buscaminas/src/Celda.java
+++ Buscaminas/src/Celda.java
@@ -1,2 +1,35 @@
-public class Celda {
+class Celda {
+    private boolean esMina;
+    private boolean revelada;
+    private int minasAlrededor;
+
+    public Celda() {
+        this.esMina = false;
+        this.revelada = false;
+        this.minasAlrededor = 0;
+    }
+
+    public boolean esMina() {
+        return esMina;
+    }
+
+    public void colocarMina() {
+        esMina = true;
+    }
+
+    public boolean estaRevelada() {
+        return revelada;
+    }
+
+    public void revelar() {
+        revelada = true;
+    }
+
+    public int getMinasAlrededor() {
+        return minasAlrededor;
+    }
+
+    public void setMinasAlrededor(int minas) {
+        this.minasAlrededor = minas;
+    }
 }
